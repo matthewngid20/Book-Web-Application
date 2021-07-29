c6b42f2360fba4955b9e056001e5bac08c928d19
matthewngid20 <matthew.ng.id20@gmail.com>
2021-07-29 15:11:30 +1000

    new changes

--- build/php/src/Books.php
+++ build/php/src/Books.php
@@ -1,37 +1,36 @@
 <?php
-
 namespace bookstore;
 
 use bookstore\Database;
 
 class Books extends Database {
 
-    //private $connection;
+  public function __construct() {
+    parent::__construct();
+  }
 
-    public function __construct() {
-        parent:: __construct();
-        //$this -> connection = Database.connection;
-        
+  public function getBooks() {
+    if( !$this -> connection ) {
+      echo "error";
+      exit();
     }
-
-    public function getBooks(){
-        $query = "SELECT * FROM book";
-        $statement = $this -> connection -> prepare($query);  
-        if( $statement -> execute() == false){
-            echo "query or database error";
-        }
-        else {
-            $result = $statement -> get_result();
-            $books = array();
-            //loop throu result to add each book 
-            while($row = $result -> fetch_assoc() ){
-                array_push($books, $row ); 
-            }    
-            return $books;
-        }
-
+    $query = "SELECT * FROM book";
+    $statement = $this -> connection -> prepare($query);
+    if( $statement -> execute() == false ) {
+      echo "query or database error";
     }
-
+    else {
+      $result = $statement -> get_result();
+      $books = array();
+      // loop through the result to add each book
+      // row represents a row in the book table
+      // fetch_assoc() converts a row into an associative array
+      // example ["book_id" => 1, "book_title" => "Save the Cat!"]
+      while( $row = $result -> fetch_assoc() ) {
+        array_push( $books, $row );
+      }
+      return $books;
+    }
+  }
 }
-
 ?>
